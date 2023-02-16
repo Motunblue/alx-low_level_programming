5d7326d5bd266c187562d52f2b2321005c20d497
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-16 05:21:15 -0800

    Modified puts3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 /**
  * main - Prints "Programming is like building a multilingual puzzle"
  * Return: 0
- * /
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
