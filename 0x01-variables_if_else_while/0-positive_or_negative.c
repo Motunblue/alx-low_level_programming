cbf812fe0d510cea07637ee217ff2b4371780528
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-17 10:11:42 -0800

    Modified positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,19 +4,26 @@
 
 /**
  * main - Prints whether random  number is positive or negative
- * @n: Randon number generated
  *
  * Return: 0 if success
  */
 int main(void)
 {
-	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	int n = rand() - RAND_MAX / 2;
+
 	if (n < 0)
+	{
 		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
 	else
+	{
 		printf("%d is positive\n", n);
+	}
 	return (0);
 }
