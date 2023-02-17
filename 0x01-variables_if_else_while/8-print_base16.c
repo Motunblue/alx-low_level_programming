d09481158741cf6c1933a7af6983fbccefd91826
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-17 13:56:43 -0800

    Added print base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all numbers in base 16
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+		putchar(i);
+	for (i = 97; i <= 102; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
