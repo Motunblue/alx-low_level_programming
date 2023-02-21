cbddfa31f58fe49643de46011733a8b39b102ae0
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-21 06:12:37 -0800

    Added isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a character is in upper or lowwer case
+ * @c: Character to be checked
+ *
+ * Return: 1 if true and 0 if false
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || c >= 'A' && c 'Z')
+		return (1);
+	else
+		return (0);
+}
