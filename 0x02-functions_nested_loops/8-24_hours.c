6711b8c7bd2e02e422d1932f635031eecc01a0e6
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-21 10:48:34 -0800

    Added 24 hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print all the minutes of the day
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int x, y;
+
+
+	for (x = 0; x < 24; x++)
+	{
+		for (y = 0; y < 60; y++)
+		{
+			_putchar(x / 10 + '0');
+			_putchar(x % 10 + '0');
+			_putchar(':');
+			_putchar(y / 10 + '0');
+			_putchar(y % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
