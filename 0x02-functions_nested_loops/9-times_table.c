04ec9156d5c31a75cab74636dbc0a346b107d774
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-21 11:31:51 -0800

    Added times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - prints the multiplication table
+ *
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int x, y, product;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			product = x * y;
+
+			if (product >= 10)
+			{
+				_putchar(product / 10 + '0');
+				_putchar(product % 10 + '0');
+			}
+			else if (y == 0)
+				_putchar(product + '0');
+			else
+			{
+				_putchar(' ');
+				_putchar(product = '0');
+			}
+			if (y != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+}
