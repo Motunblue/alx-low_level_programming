cd451b2a9caf12636789f72713eaf35628185265
Motunblue <ajirotutu4real@gmail.com>
2023-02-21 10:41:54 +0100

    Added putcharc

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints some strings
+ *
+ * Return: 1 if success
+ */
+
+int main(void)
+{
+	int j;
+	char *i = "_putchar";
+
+	for(j = 0 ; j <= 7 ; j++)
+		putchar(i[j]);
+	putchar('\n');
+}
