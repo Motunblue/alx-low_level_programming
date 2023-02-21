ede00dd149c146080f1564a61b07a34e7a228776
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-21 11:44:40 -0800

    Added add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds 2 numbers
+ * @a: number 1
+ * @b: number 2
+ *
+ * Return: the result
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
