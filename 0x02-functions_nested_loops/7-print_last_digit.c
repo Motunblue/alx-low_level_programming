1663438de4e5a2e6fb9418916262eb513a865184
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-21 09:43:20 -0800

    Added printlastDigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Calculate last digit of a number
+ * @n: number to be checked
+ *
+ * Return: the result
+ */
+
+int _abs(int n)
+{
+	int a;
+
+	a = n % 10;
+
+	return (a);
+}
