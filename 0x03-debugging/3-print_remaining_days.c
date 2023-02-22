2aaab9d0314a0e4573cb808c774b17fb6fcedc99
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-22 10:17:51 -0800

    Modified remaining days

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,9 +24,9 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60)
+		if (month == 2 && day > 59)
 		{
-			day--;
+			day = 59;
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
