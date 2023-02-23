1302fe72374af8f8a19d7deb3c4f990c2384cc79
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-23 10:41:03 -0800

    Added print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square
+ * @size: size of square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < n; j++)
+			_putchar('_');
+	_putchar('\n');
+	}
+}
