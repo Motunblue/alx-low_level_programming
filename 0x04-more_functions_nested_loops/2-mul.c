e70c2b61e875a6829c9ad76e5c20868d78db59c0
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-23 07:13:38 -0800

    Added files

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integer
+ * @a: Integer 1
+ * @b: Integer 2
+ *
+ * Return: result
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
