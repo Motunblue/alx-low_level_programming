850ff4b4148665a09ad52f2ead16a60c0cdcc1d8
Motunrayo Ajirotutu <moajirotutu@gmail.com>
2023-02-27 13:53:51 -0800

    Added files

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - reset a value to point to 98
+ * 
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Working with pointer and arrays
