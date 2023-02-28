903de16e272b2f6c67977172619887311aaac4da
Motunrayo <ajirotutu4real@gmail.com>
2023-02-28 15:46:42 +0000

    Modified prit rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,9 +2,29 @@
 
 /**
  * print_rev - prints a string in reverse
+ * @s: string to reverse
  *
- * s: @string to reverse
  * Return : void
  */
 
 void print_rev(char *s)
+{
+	char *p, *q;
+
+	p = s;
+	q = s;
+
+	while (*q != '\0')
+	{
+		p++;
+		q++;
+	}
+
+	while (p != s)
+	{
+		_putchar(*p);
+		p--;
+	}
+	_purchar(*s);
+	_putchar('\n');
+}
