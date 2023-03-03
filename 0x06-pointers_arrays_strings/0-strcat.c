89a901105ed5b24c162ee83a821ddc029cdcf3bb
Motunrayo <ajirotutu4real@gmail.com>
2023-03-03 00:00:35 +0000

    Modified strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,19 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concertanate two strings
+ * @dest: string 1
+ * @src: string 2
+ *
+ * Return: value of string 1 which is a pointer to the result
+ */
+
 char *_strcat(char *dest, char *src)
 {
-	  int i, j;
+	int i, j;
 
-	    i = 0;
-	      j = 0;
+	i = 0;
+	j = 0;
 
-	        while (dest[j] != '\0')
-			    {
-				          j++;
-					      }
-		  while (src[i] != '\0')
-			      {
-				            dest[j] = src[i];
-					          i++;
-						        j++;
-							    }
-		      dest[j] = src[i];
-		        return (dest);
+	while (dest[j] != '\0')
+	{
+		j++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = src[i];
+	return (dest);
+}
