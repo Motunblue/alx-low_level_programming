37687882be4a8510c33e473034c946e5b2d3cf70
Motunrayo <ajirotutu4real@gmail.com>
2023-03-30 15:52:51 +0000

    Modified strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,9 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j = 0;
 
-	for (; src[j] != '\0' && j < n - 1; j++)
+	for (; src[j] != '\0' && j < n; j++)
 		dest[j] = src[j];
-	if (j < n)
+	if (i < n)
 		dest[j] = '\0';
 	return (dest);
 }
