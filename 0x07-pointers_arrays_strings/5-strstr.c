443c8c6879307cab33f841d331fb9f42d33004c6
Motunrayo <ajirotutu4real@gmail.com>
2023-04-16 17:39:39 +0000

    Added strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strstr - Locates a substring
+ *
+ * @haystack: Pointer to main string
+ * @needle: Pointer to substring
+ *
+ * Return: Pointer to locate substring is present or null
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, same;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		same = 0;
+		j = 0;
+		if (needle[j++] == haystack[i])
+		{
+			for (; needle[j] != '\0'; j++)
+			{
+				if (needle[j] == haystack[i + j])
+				same = 1;
+				else
+				{
+					same = 0;
+					break;
+				}
+			}
+		}
+		if (same)
+		return (haystack + i);
+	}
+	return (NULL);
+}
