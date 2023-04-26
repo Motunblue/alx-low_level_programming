6a0ddab0416954e661a0c0d1bb0bbf731cf69e59
Motunrayo <ajirotutu4real@gmail.com>
2023-04-26 12:31:46 +0100

    Added pow recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Calculate the power of a number
+ * @x: Number to power
+ * @y: Number to ve powered to
+ *
+ * Return: Power of the number x ^ y or -1 if error
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+		return(x * _pow_recursion(x, y - 1));
+
+}
