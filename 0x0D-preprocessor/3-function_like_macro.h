1bc0c75819cd98823c612e8263e09876c9557c0e
Motunrayo <ajirotutu4real@gmail.com>
2023-06-05 19:08:37 +0000

    Modified func like macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,3 +1,8 @@
+#ifndef _FUNCTION_LIKE_MACRO_H_
+#define _FUNCTION_LIKE_MACRO_H_
+
 #ifndef ABS
-#define ABS(X) ((X) > (0) ? (X) : (X * -1))
+#define ABS(X) ((X) < (0) ? (X * -1) : (X))
+#endif
+
 #endif
