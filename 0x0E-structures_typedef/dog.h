f1ebaf4c2d244bc3f41d7d85d0a7675fb8dad97c
Motunrayo <ajirotutu4real@gmail.com>
2023-06-06 18:26:46 +0000

    Added do.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - type for a dog
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Doge owner
+ *
+ * Description: This is a type that can hold all attributable
+ * character of a dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
