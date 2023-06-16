655dd7222ebfd3fe0a7bf93e612b1ff0289656c6
Motunrayo <ajirotutu4real@gmail.com>
2023-06-16 15:19:34 +0000

    Modified string nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,10 +31,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (i = 0; i < count_s1; i++)
 			s[i] = s1[i];
 		s[i] = '\0';
+		return (s);
 	}
 	for (i = 0; s2[i] != '\0'; i++)
 		count_s2++;
-	if (n >= count_s2)
+	if (n > count_s2)
 		size = count_s2;
 	else
 		size = n;
