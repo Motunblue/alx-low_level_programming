1ea1a1e0bb83273bb735f5a1c0e70fffd769eff6
Motunrayo <ajirotutu4real@gmail.com>
2023-06-17 22:46:58 +0000

    Modified 101 print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,30 +1,43 @@
 #include "main.h"
 
-/** print number - Prints an integer
+/**
+ * print_number - Prints an integer
  * @n: integer to print
  */
 
 void print_number(int n)
 {
-	int x;
-	int y = 0, z, h;
+	int x = n, p = n;
+	int y = 0, z, h = 1;
 	char c;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		while (p < -10)
+		{
+			p = p / 10;
+			y += 1;
+		}
+		for (; y > 0; y--)
+		{
+			for (h = 1, z = y; z > 0; z--)
+				h = h * 10;
+			c = (((n / h) % 10) * -1 + 48);
+			_putchar(c);
+		}
+		_putchar((n % 10) * -1 + 48);
+		return;
 	}
-	x = n;
 	while (x >= 10)
 	{
 		x = x / 10;
 		y += 1;
 	}
-		
+
 	for (; y > 0; y--)
 	{
-		for (h = 1, z = y; z > 0; z --)
+		for (h = 1, z = y; z > 0; z--)
 			h = h * 10;
 		c = (((n / h) % 10) + 48);
 		_putchar(c);
