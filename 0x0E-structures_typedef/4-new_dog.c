2b388fdac632d4c9d0f30e06319d9befa493c4d0
Motunrayo <ajirotutu4real@gmail.com>
2023-06-19 19:11:14 +0000

    Modified 5-free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,20 +18,26 @@ dog_t *new_dog(char *name, float age, char *owner)
 	s = malloc(sizeof(dog_t));
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; name[i] != '\0'; i++)
-		size++;
-	s->name = malloc(sizeof(char) * (size + 1));
-	for (i = 0; name[i] != '\0'; i++)
-		s->name[i] = name[i];
-	s->name[i] = '\0';
+	if (name != NULL)
+	{
+		for (i = 0; name[i] != '\0'; i++)
+			size++;
+		s->name = malloc(sizeof(char) * (size + 1));
+		for (i = 0; name[i] != '\0'; i++)
+			s->name[i] = name[i];
+		s->name[i] = '\0';
+	}
 	s->age = age;
 	size = 0;
-	for (i = 0; owner[i] != '\0'; i++)
-		size++;
-	s->owner = malloc(sizeof(char) * (size + 1));
-	for (i = 0; owner[i] != '\0'; i++)
-		s->owner[i] = owner[i];
-	s->owner[i] = '\0';
+	if (owner != NULL)
+	{
+		for (i = 0; owner[i] != '\0'; i++)
+			size++;
+		s->owner = malloc(sizeof(char) * (size + 1));
+		for (i = 0; owner[i] != '\0'; i++)
+			s->owner[i] = owner[i];
+		s->owner[i] = '\0';
+	}
 
 	return (s);
 }
