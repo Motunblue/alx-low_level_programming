f9cd9eda681a42e701a3602005c9aef5165d4611
Motunrayo <ajirotutu4real@gmail.com>
2023-06-21 08:31:14 +0100

    Added function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ * @name: string of name to print
+ * @f: Pointer to fuction that prints name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+This directory seeks problems from the ALX SE 0x0F-function_pointers
