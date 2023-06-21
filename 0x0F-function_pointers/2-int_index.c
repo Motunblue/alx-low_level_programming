a4448552dfaa2112097b9c8556fe86939f59f6c0
Motunrayo <ajirotutu4real@gmail.com>
2023-06-21 18:40:53 +0000

    Added 2-int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Searchs for an integer
+ * @array: Array of integer
+ * @size: size of the array
+ * @cmp: Pointer to search function
+ *
+ * Return: index of the first element
+ * -1 if no element matches or size less than 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL && cmp == NULL)
+		return (-1);
+	if (size < 1)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 1)
+			return (i);
+	}
+
+	return (-1);
+}
