b3dbdd71edff1a486179aa247b4614be5f2fb2fe
Motunrayo <ajirotutu4real@gmail.com>
2023-06-22 15:21:56 +0000

    Added print_strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -22,7 +22,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		s = va_arg(list, char*);
 
 		if (!s)
-			printf("(nill)");
+			printf("(nil)");
 		else
 			printf("%s", s);
 		if (separator && i < (n - 1))
