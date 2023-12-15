59ab458830df95166d3b3b6ba829639b020cc440
Motun <ajirotutu4real@gmail.com>
2023-12-15 23:04:18 +0100

    Modified 102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,5 +10,9 @@
 */
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]))
+	int low, high, i;
+
+	size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+
+	return (i);
 }
